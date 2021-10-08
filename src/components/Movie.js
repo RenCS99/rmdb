9d3e6e0483cc1b38050482eb01ca37f016857e19
RenCS99 <rcuevasalas99@gmail.com>
2021-10-08 14:30:16 -0700

    Changed functional comp to class comp

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { useParams } from 'react-router-dom';
 
 // Config
@@ -12,41 +12,79 @@ import MovieInfo from './MovieInfo';
 import MovieInfoBar from './MovieInfoBar';
 import Actor from './Actor';
 
-// Hook
-import { useMovieFetch } from '../hooks/useMovieFetch';
-
 // Image
 import NoImage from '../images/no_image.jpg';
+import API from '../API';
 
-const Movie = () => {
-    const { movieId } = useParams();
+class Movie extends Component {
 
-    const { state: movie, loading, error } = useMovieFetch(movieId);
+    state = {
+        movie: {},
+        loading: true, 
+        error: false
+    };
 
-    if (loading) return <Spinner />;
-    if (error) return <div>Something went wrong...</div>;
+     fetchMovie = async () => {
+        const { movieId } = this.props.params;
 
-    return (
-        <>
-            <BreadCrumb movieTitle={movie.original_title} />
-            <MovieInfo movie={movie} />
-            <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
-            <Grid header='Actors'>
-                {movie.actors.map(actor => (
-                    <Actor 
-                        key={actor.credit_id}
-                        name={actor.name}
-                        character={actor.character}
-                        imageUrl={
-                            actor.profile_path 
-                            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
-                            : NoImage
-                        }
-                    />
-                ))};
-            </Grid>
-        </>
-    );
+        try {
+            this.setState({ error: false, loading: true });
+
+            const movie = await API.fetchMovie(movieId);
+            const credits = await API.fetchCredits(movieId);
+
+            // Get directors only
+            const directors = credits.crew.filter(
+                member => member.job === 'Director'
+            );
+
+            this.setState({
+                movie: {
+                    ...movie, 
+                    actors: credits.cast,
+                    directors
+                },
+                loading: false,
+            });
+        } catch (error) {
+            this.setState({ error: true, loading: false });
+        }
+    };
+
+    componentDidMount() {
+        this.fetchMovie();
+    }
+
+    render() {
+
+        const { movie, loading, error } = this.state;
+
+        if (loading) return <Spinner />;
+        if (error) return <div>Something went wrong...</div>;
+
+        return (
+            <>
+                <BreadCrumb movieTitle={movie.original_title} />
+                <MovieInfo movie={movie} />
+                <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
+                <Grid header='Actors'>
+                    {movie.actors.map(actor => (
+                        <Actor 
+                            key={actor.credit_id}
+                            name={actor.name}
+                            character={actor.character}
+                            imageUrl={
+                                actor.profile_path 
+                                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                                : NoImage
+                            }
+                        />
+                    ))};
+                </Grid>
+            </>
+        );
+    }    
 };
 
-export default Movie;
+const  MovieWithParams = props => <Movie { ...props } params={useParams()} />
+export default MovieWithParams;
